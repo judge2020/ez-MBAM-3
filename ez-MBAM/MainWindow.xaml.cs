e84d2da055e78b67c07840c048e8cfd3dd8bda48
judge2020 <hunterray55@hotmail.com>
2016-12-22 12:44:51 -0500

    KHG sound

--- ez-MBAM/MainWindow.xaml.cs
+++ ez-MBAM/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,11 +22,34 @@ namespace ez_MBAM
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : MetroWindow
+	public partial class MainWindow
 	{
+		readonly SoundPlayer _khgSound = new SoundPlayer(Assembly.GetExecutingAssembly().GetManifestResourceStream("ez_MBAM.khg.wav"));
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			var maintimer = new Timer(1000) {AutoReset = true};
+			maintimer.Elapsed += MaintimerOnElapsed;
+			_khgSound.PlayLooping();
+			_khgSound.Stop();
+
+		}
+
+		private void MaintimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+		{
+			
+		}
+
+		private void checkBox_Checked(object sender, RoutedEventArgs e)
+		{
+			_khgSound.PlayLooping();	
+			
+		}
+
+		private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			_khgSound.Stop();
 		}
 	}
 }
