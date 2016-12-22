133b31b8c01a356d2afcd4c7b80b9627b759816e
judge2020 <hunterray55@hotmail.com>
2016-12-22 13:38:29 -0500

    core functions work

--- ez-MBAM/MainWindow.xaml.cs
+++ ez-MBAM/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Media;
 using System.Reflection;
@@ -16,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using Path = System.IO.Path;
 
 namespace ez_MBAM
 {
@@ -24,32 +27,74 @@ namespace ez_MBAM
 	/// </summary>
 	public partial class MainWindow
 	{
-		readonly SoundPlayer _khgSound = new SoundPlayer(Assembly.GetExecutingAssembly().GetManifestResourceStream("ez_MBAM.khg.wav"));
+		readonly SoundPlayer _khgSound = new SoundPlayer(Properties.Resources.khg);
 
 		public MainWindow()
 		{
 			InitializeComponent();
 			var maintimer = new Timer(1000) {AutoReset = true};
 			maintimer.Elapsed += MaintimerOnElapsed;
-			_khgSound.PlayLooping();
 			_khgSound.Stop();
 
 		}
 
 		private void MaintimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
 		{
-			
+			if (Directory.Exists(@"C:\Program Files\Malwarebytes"))
+			{
+				Status_label.Content = "Malwarebyted 3 Found!";
+			}
 		}
 
 		private void checkBox_Checked(object sender, RoutedEventArgs e)
 		{
 			_khgSound.PlayLooping();	
+
 			
 		}
 
 		private void checkBox_Unchecked(object sender, RoutedEventArgs e)
 		{
 			_khgSound.Stop();
+		}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+			string tempExeName = Path.Combine(Path.GetTempPath(), "mbam218.exe");
+			using(FileStream fsDst = new FileStream(tempExeName, FileMode.CreateNew, FileAccess.Write))
+			{
+				byte[] bytes = Properties.Resources.mbam218;
+
+				fsDst.Write(bytes, 0, bytes.Length);
+				fsDst.Close();
+			}
+			Process.Start(tempExeName);
+		}
+
+		private void button_Copy2_Click(object sender, RoutedEventArgs e)
+		{
+			string tempExeName = Path.Combine(Path.GetTempPath(), "activation218.exe");
+			using(FileStream fsDst = new FileStream(tempExeName, FileMode.CreateNew, FileAccess.Write))
+			{
+				byte[] bytes = Properties.Resources.activation218;
+
+				fsDst.Write(bytes, 0, bytes.Length);
+				fsDst.Close();
+			}
+			Process.Start(tempExeName);
+		}
+
+		private void button2_Click(object sender, RoutedEventArgs e)
+		{
+			string tempExeName = Path.Combine(Path.GetTempPath(), "mbam3.exe");
+			using(FileStream fsDst = new FileStream(tempExeName, FileMode.CreateNew, FileAccess.Write))
+			{
+				byte[] bytes = Properties.Resources.mbam3;
+
+				fsDst.Write(bytes, 0, bytes.Length);
+				fsDst.Close();
+			}
+			Process.Start(tempExeName);
 		}
 	}
 }
